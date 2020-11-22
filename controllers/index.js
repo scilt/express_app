89859c6790a965483ab50a9e16f3bf3f302e663c
scilt <suncenter1914@gmail.com>
2020-11-22 13:35:10 +0800

    added desc of index

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 module.exports.controller = (app) => {
     // get homepage
     app.get('/', (req, res) => {
-        res.render('index', { title: 'Express' });
-    })
+        res.render('index', { title: 'Express', description: 'This is the description of the index' });    })
    }
    
